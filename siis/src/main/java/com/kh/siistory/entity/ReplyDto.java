71486e18bd2f3eb73ff237604efce8ca37f2bc45
user1 <user1@DESKTOP-0IFG55Q>
2020-01-31 17:34:21 +0900

    댓글기능 구현중

---
+++ siis/src/main/java/com/kh/siistory/entity/ReplyDto.java
@@ -0,0 +1,15 @@
+package com.kh.siistory.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReplyDto {
+	int reply_no, board_no, member_no, super_no, group_no, depth, reply_count;
+	String reply_wdate, reply_content;
+}
