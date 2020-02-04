0ea96794e212a4a0da3c8a9d6b4cc6c008b7c2a2
user1 <user1@DESKTOP-0IFG55Q>
2020-02-04 16:19:13 +0900

    dashboard 구현중

--- siis/src/main/java/com/kh/siistory/controller/DashBoardController.java
+++ siis/src/main/java/com/kh/siistory/controller/DashBoardController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,29 +12,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.siistory.entity.BoardDto;
 import com.kh.siistory.repository.BoardDao;
-import com.kh.siistory.vo.SeqVo;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashBoardController {
 	@Autowired
 	private BoardDao boardDao;
-	
-	@GetMapping("/")
-	public String dashboard() {
+
+	@GetMapping({ "/", "" })
+	public String dashboard(Model model) {
+		model.addAttribute("list", boardDao.dashboardlist());
 		return "dashboard/dashboard";
 	}
-	
+
 	@GetMapping("/write")
 	public String write() {
 		return "dashboard/write";
 	}
-	
+
 	@PostMapping("/write")
 	public String write(@ModelAttribute BoardDto boardDto, HttpSession session) {
 		boardDao.setWrtie(boardDto, session);
 		return "redirect:/";
 	}
-	
+
 //	@GetMapping()
 }
--- siis/src/main/java/com/kh/siistory/repository/BoardDao.java
+++ siis/src/main/java/com/kh/siistory/repository/BoardDao.java
@@ -1,5 +1,7 @@
 package com.kh.siistory.repository;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import com.kh.siistory.entity.BoardDto;
@@ -8,4 +10,5 @@ import com.kh.siistory.vo.SeqVo;
 public interface BoardDao {
 	SeqVo getSequence();
 	void setWrtie(BoardDto boardDto, HttpSession session);
+	List<BoardDto> dashboardlist();
 }
