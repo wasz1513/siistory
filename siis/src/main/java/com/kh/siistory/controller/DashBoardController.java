1b2b0f609328cc6c89d54540590ba5be161bcbf3
user1 <user1@DESKTOP-0IFG55Q>
2020-02-05 15:04:44 +0900

    댓글 insert 구현중

--- siis/src/main/java/com/kh/siistory/controller/DashBoardController.java
+++ siis/src/main/java/com/kh/siistory/controller/DashBoardController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.siistory.entity.BoardDto;
+import com.kh.siistory.entity.ReplyDto;
 import com.kh.siistory.repository.BoardDao;
+import com.kh.siistory.repository.ReplyDao;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,6 +23,9 @@ import lombok.extern.slf4j.Slf4j;
 public class DashBoardController {
 	@Autowired
 	private BoardDao boardDao;
+	
+	@Autowired
+	private ReplyDao replyDao;
 
 	@GetMapping({ "/", "" })
 	public String dashboard(Model model) {
@@ -39,5 +44,8 @@ public class DashBoardController {
 		return "redirect:/";
 	}
 
-//	@GetMapping()
+	@PostMapping("/replyinsert")
+	public void replywrite(@ModelAttribute ReplyDto replyDto) {
+		replyDao.insert(replyDto);
+	}
 }
--- siis/src/main/java/com/kh/siistory/repository/ReplyDaoImpl.java
+++ siis/src/main/java/com/kh/siistory/repository/ReplyDaoImpl.java
@@ -1,26 +1,19 @@
 package com.kh.siistory.repository;
 
-import java.util.List;
-
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.kh.siistory.entity.BoardDto;
 import com.kh.siistory.entity.ReplyDto;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Repository
-@Slf4j
 public class ReplyDaoImpl implements ReplyDao {
 	@Autowired
 	private SqlSession sqlSession;
-	
+
 	@Override
-	public List<ReplyDto> replylist(int no) {
-		List<ReplyDto> list = sqlSession.selectList("reply.list", no);
-		return list;
+	public void insert(ReplyDto replyDto) {
+		
 	}
 
 }
