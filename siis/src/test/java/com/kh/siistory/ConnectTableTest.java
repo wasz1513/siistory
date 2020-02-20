e4b009f2aa7fe60fd5d6c3c52dcd89d8d391cdf7
tjrnrghks <skhfofo1004@naver.com>
2020-02-20 18:12:24 +0900

    connectTable 접속 통계 기본 구조 틀 작성

---
+++ siis/src/test/java/com/kh/siistory/ConnectTableTest.java
@@ -0,0 +1,36 @@
+package com.kh.siistory;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.kh.siistory.repository.ConnectTableDao;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+@Slf4j
+public class ConnectTableTest {
+
+	@Autowired
+	private ConnectTableDao connecttableDao;
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+
+	@Test
+	public void new_session_test () {
+		
+		sqlSession.update("connect.newsession");
+		
+	}
+	
+	
+}
