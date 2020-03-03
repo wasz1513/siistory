ee9e16c9014e6086d26f5bca6b16f80643878064
tjrnrghks <skhfofo1004@naver.com>
2020-03-03 20:46:14 +0900

    좋아요  total 카운트 변경 페이크 처리

--- siis/src/main/webapp/resources/js/messenger.js
+++ siis/src/main/webapp/resources/js/messenger.js
@@ -314,13 +314,6 @@ $(function() {
 		var content_no = $(this).data(content_no).content_no;
 		var content_type = $(this).data(content_type).content_type;
 		var content_play = $(this).data(content_play).content_play;
-//		console.log($(this).data(member_no).member_no);
-		console.log ("member_no = " + member_no);
-		console.log ("target_no = " + target_no);
-		console.log ("pusher_no = " + pusher_no);
-		console.log ("content_no = " + content_no);
-		console.log ("content_type = " + content_type);
-		console.log ("content_play = " + content_play);
 		
 		if($(this).text()=="좋아요"){
 		
@@ -331,23 +324,17 @@ $(function() {
 		$(this).attr("class","btn good-btn good-off").text("좋아요 취소");
 
 		$(this).next().next().text(num)
-		console.log(i);	
-		
-		
+
 		} else if ($(this).text()=="좋아요 취소"){
 
 		var i= $(this).next().next().text();
 		var num = parseInt(i)-1;
-		
-		console.log(i)
-		console.log(num)
+
 		send_alarm(member_no, 5, target_no, pusher_no, content_no, content_type, content_play)  // 취소
 		$(this).attr("class","btn good-btn good-on").text("좋아요");
-		
-		
-		
+	
 		$(this).next().next().text(num)
-		console.log(i);
+
 		}
 		
 		
